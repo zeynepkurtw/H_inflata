330e82eda35712d68f4e01483d605e59eb1640f8
zeynepkurtw <zeynepkurt.w@gmail.com>
2022-10-17 16:14:16 +0200

    h.inflata

--- scripts/cdhit.py
+++ scripts/cdhit.py
@@ -3,8 +3,8 @@ from snakemake.shell import shell
 input = snakemake.input
 out = snakemake.output
 
-seq_identity = snakemake.param.seq_identity
-threads = snakemake.param.threads
+seq_identity = snakemake.params.seq_identity
+threads = snakemake.params.threads
 
 
 shell(f"""cd-hit -i {input} -o {out} -c {seq_identity} -T {threads}""")
