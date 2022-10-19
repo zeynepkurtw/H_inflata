22e8d7c4a0caee5dcdf988c87cae60a43764c213
zeynepkurtw <zeynepkurt.w@gmail.com>
2022-10-19 16:32:10 +0200

    h.inflata

--- scripts/interproscan.py
+++ scripts/interproscan.py
@@ -5,5 +5,5 @@ output_folder = snakemake.params.output_folder
 
 threads = snakemake.params.threads
 
-shell(f"""sed 's/*//' resource/3_interproscan/new_sp/trepo_aa.fasta > resource/3_interproscan/new_sp/trepo_aa.fasta """)
-shell(f"""/data/zeynep/interproscan-5.47-82.0/interproscan.sh -i {input} -d {output_folder} -f gff3,tsv,json -iprlookup -goterms --pathways -cpu {threads} """)
+#sed 's/*//' trepo.faa > trepo_aa.fasta
+shell(f"""/data/zeynep/interproscan-5.47-82.0/intercd ../.proscan.sh -i {input} -d {output_folder} -f gff3,tsv,json -iprlookup -goterms --pathways -cpu {threads} """)
