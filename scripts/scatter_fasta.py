9420123b68eafe0f4a8c700a9167b609eadfacd4
zeynepkurtw <zeynepkurt.w@gmail.com>
2023-01-24 18:35:14 +0100

    95

--- scripts/scatter_fasta.py
+++ scripts/scatter_fasta.py
@@ -11,14 +11,14 @@ except NameError:
     partition_fasta = f"data/LGT_search/blast_in/partition/partition.fasta"
 
     n_partitions = 32
-    n_partition = 0
+    i_partition = 0
 
 
 with open(multi_fasta, "r") as fasta_in:
     with open(partition_fasta, "w") as fasta_out:
         i, j = 0, 0
         for i, seq in enumerate(SeqIO.parse(fasta_in, "fasta")):
-            if i % n_partitions == n_partition:
+            if i % n_partitions == i_partition:
                 SeqIO.write([seq], fasta_out, "fasta")
                 j += 1
             i += 1
